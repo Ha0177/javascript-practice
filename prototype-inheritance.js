8de0013335f6dddd0afdedf57d3c154e1eae37a3
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-07-17 16:39:58 +0300

    Add ex

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -28,23 +28,39 @@
 // player2.getMarker();
 
 
-let head = {
-    glasses: 1
-  };
+// let head = {
+//     glasses: 1
+//   };
   
-  let table = {
-    pen: 3,
-  };
+//   let table = {
+//     pen: 3,
+//   };
   
-  let bed = {
-    sheet: 1,
-    pillow: 2
-  };
+//   let bed = {
+//     sheet: 1,
+//     pillow: 2
+//   };
   
-  let pockets = {
-    money: 2000
-  };
+//   let pockets = {
+//     money: 2000
+//   };
 
-  Object.setPrototypeOf(table, head);
-  Object.setPrototypeOf(bed, table);
-  Object.setPrototypeOf(pockets, bed);
+//   Object.setPrototypeOf(table, head);
+//   Object.setPrototypeOf(bed, table);
+//   Object.setPrototypeOf(pockets, bed);
+
+let animal = {
+    eat() {
+        this.full = true;
+    }
+};
+
+let rabbit = {
+
+};
+
+Object.setPrototypeOf(rabbit, animal);
+
+rabbit.eat();
+console.log(rabbit)
+console.log(animal)
