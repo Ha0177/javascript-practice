e7a307940850d43e554fb55c8b22d6e294bc994c
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-07-17 16:35:13 +0300

    Add ex

--- prototype-inheritance.js
+++ prototype-inheritance.js
@@ -1,28 +1,50 @@
 
-function Person(name) {
-    this.name = name;
-}
+// function Person(name) {
+//     this.name = name;
+// }
 
-Person.prototype.sayName = function() {
-    console.log(`Hello, I'm ${this.name}!`);
-};
+// Person.prototype.sayName = function() {
+//     console.log(`Hello, I'm ${this.name}!`);
+// };
 
-function Player(name, marker) {
-    this.name = name;
-    this.marker = marker;
-}
+// function Player(name, marker) {
+//     this.name = name;
+//     this.marker = marker;
+// }
 
-Player.prototype.getMarker = function() {
-    console.log(`My marker is '${this.marker}'`);
-};
+// Player.prototype.getMarker = function() {
+//     console.log(`My marker is '${this.marker}'`);
+// };
 
-Object.setPrototypeOf(Player.prototype, Person.prototype);
+// Object.setPrototypeOf(Player.prototype, Person.prototype);
 
-const player1 = new Player("steve", "X");
-const player2 = new Player("also steve", "O")
+// const player1 = new Player("steve", "X");
+// const player2 = new Player("also steve", "O")
 
-player1.sayName();
-player2.sayName();
+// player1.sayName();
+// player2.sayName();
 
-player1.getMarker();
-player2.getMarker();
+// player1.getMarker();
+// player2.getMarker();
+
+
+let head = {
+    glasses: 1
+  };
+  
+  let table = {
+    pen: 3,
+  };
+  
+  let bed = {
+    sheet: 1,
+    pillow: 2
+  };
+  
+  let pockets = {
+    money: 2000
+  };
+
+  Object.setPrototypeOf(table, head);
+  Object.setPrototypeOf(bed, table);
+  Object.setPrototypeOf(pockets, bed);
